f7f683a9cd2d6e13a9071becbdfaf562aa582c7d
HULYOWX <juliosofiotare@gmail.com>
2025-06-05 12:42:40 +0800

    Delete task1.cs

--- task1.cs
+++
@@ -1,5 +0,0 @@
-﻿Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
-Console.WriteLine("Hello, World!");
